2b9f9aceb8c9ac0aa89d9e707b9bb44b70de07d1
王睿 <wangrui027@outlook.com>
2026-03-11 10:10:57 +0800

    提交 metadata.yaml、logo、示例代码

---
+++ main.py
@@ -0,0 +1,19 @@
+from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
+from astrbot.api.star import Context, Star, register
+from astrbot.api import logger # 使用 astrbot 提供的 logger 接口
+
+class MyPlugin(Star):
+    def __init__(self, context: Context):
+        super().__init__(context)
+
+    # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
+    @filter.command("dlna-cast")
+    async def dlna_cast(self, event: AstrMessageEvent):
+        '''这是一个 hello world 指令''' # 这是 handler 的描述，将会被解析方便用户了解插件内容。非常建议填写。
+        user_name = event.get_sender_name()
+        message_str = event.message_str # 获取消息的纯文本内容
+        logger.info("触发 dlna_cast 指令!")
+        yield event.plain_result(f"Hello, {user_name}, 我是 dlna-cast, 我可以把你 webdav 中的电影推送到 bilibili TV 版哟!") # 发送一条纯文本消息
+
+    async def terminate(self):
+        '''可选择实现 terminate 函数，当插件被卸载/停用时会调用。'''
