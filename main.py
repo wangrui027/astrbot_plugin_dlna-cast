9bf4077fc317e33def341eb466bc864dbb5d5c2c
王睿 <wangrui027@outlook.com>
2026-03-11 10:57:41 +0800

    提交 metadata.yaml、logo、示例代码

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ class MyPlugin(Star):
 
     # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
     @filter.command("dlna-cast")
-    async def dlna_cast(self, event: AstrMessageEvent):
+    async def dlna_cast(self, event: AstrMessageEvent, text: str):
         '''这是一个 hello world 指令''' # 这是 handler 的描述，将会被解析方便用户了解插件内容。非常建议填写。
         user_name = event.get_sender_name()
         message_str = event.message_str # 获取消息的纯文本内容
